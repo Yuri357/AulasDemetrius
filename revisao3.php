7efb3c3e2e0aed393895d44049b35fc4b23df119
Yuri357 <127751100+Yuri357@users.noreply.github.com>
2023-05-04 11:35:01 -0300

    Add files via upload

---
+++ revisao3.php
@@ -0,0 +1,23 @@
+<?php
+$valor = 1200;
+$juros = $valor * 0.12;
+$valortotal = $valor + $juros;
+$parcelas = $valortotal /12;
+
+echo $valortotal."<br>";
+
+echo $parcelas;
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ revisao4.php
@@ -0,0 +1,22 @@
+<?php
+
+$dias = 20000;
+$idade = $dias/365;
+
+echo number_format($idade);
+
+if ($idade>=18){
+echo " maior de idade ";
+
+} else {
+    echo "menor de idade ";
+
+}
+
+
+
+
+
+
+
+?>
