94bebaf8f7f3a4ad35a6d9038f0d1370dba4b886
Yuri Vitório <127751100+Yuri357@users.noreply.github.com>
2023-05-30 11:30:18 -0300

    Add files via upload

--- site/logado.php
+++ site/logado.php
@@ -6,10 +6,11 @@ $senha = isset ($_POST['senha'])?$_POST['senha']:'';
 $select ="select nome, login, senha from login where login = '$login' and senha ='$senha'";
 $query = mysqli_query($conexao,$select);
 $dados = mysqli_fetch_row($query);
-session_start();
-$_SESSION['nome'] = $dados[0];
 
-if ($login == $dados[1] && $senha == $dados[2]){    
+
+if ($login == $dados[1] && $senha == $dados[2]){ 
+    session_start();
+$_SESSION['nome'] = $dados[0];   
     header ('location:index.php');
 } else {
     header ('location:login.php');
---
+++ site/sair.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+$_SESSION['nome'] = null;
+session_destroy();
+header ('location:index.php');
+
+?>
