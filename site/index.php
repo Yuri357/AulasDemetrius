4c21fe7d34818fb74a1c5663120660d0d18580a1
Yuri Vitório <127751100+Yuri357@users.noreply.github.com>
2023-06-01 11:13:55 -0300

    Add files via upload

--- site/index.php
+++ site/index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" type = "text/css" href = "estilo.css">
     <title>Document</title>
 </head>
 <body>
@@ -16,11 +17,11 @@ olá,
 if(isset($_SESSION['nome'])== null){
 ?>
   visitante<br>
+  <a href="cadastro.php">Cadastrar</a><br>
   <a href ="login.php">login</a>
 <?php } else {
     echo $_SESSION['nome'];?>
     <br>
-<a href="cadastro.php">Cadastrar</a><br>
 <a href="sair.php">Sair</a>
 <?php } ?>
 </center>
