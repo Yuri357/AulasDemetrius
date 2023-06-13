ef929e4ddeaa608f221af06399f5706c90fb7548
Yuri Vitório <127751100+Yuri357@users.noreply.github.com>
2023-06-13 11:06:41 -0300

    Add files via upload

---
+++ site/altsenha.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<form method="post" action="alterarsenha.php">
+  <label for="senha_atual">Senha Atual:</label>
+  <input type="password" id="senha_atual" name="senha_atual">
+  
+  <label for="nova_senha">Nova Senha:</label>
+  <input type="password" id="nova_senha" name="nova_senha">
+  
+  <input type="submit" value="Alterar Senha">
+</form>
+
+</body>
+</html>
